15cf46820c345eaa772c9fd45359800cd8803210
CodeBean University <codebeanuniversity@gmail.com>
2020-04-11 19:38:46 +0200

    Spring Boot Dependency download insertion

---
+++ src/main/java/nl/codebean/reactspringboot/ReactSpringBootApplication.java
@@ -0,0 +1,13 @@
+package nl.codebean.reactspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/codebean/reactspringboot/ReactSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.codebean.reactspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
