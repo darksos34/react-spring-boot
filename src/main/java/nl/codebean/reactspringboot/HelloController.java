9181885485152f26e6ad1bb2bef1cf38daf3a966
CodeBean University <codebeanuniversity@gmail.com>
2020-04-12 10:23:11 +0200

    Update HelloController.java

---
+++ src/main/java/nl/codebean/reactspringboot/HelloController.java
@@ -0,0 +1,13 @@
+package nl.codebean.reactspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @RequestMapping("/")
+  public String hello() {
+    return "Greetings from CodeBean!";
+  }
+}
